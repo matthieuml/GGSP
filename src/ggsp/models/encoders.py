0338b5cd387e4ab0c728b3df6544f443bee7223c
sachabinder <binder.sacha@gmail.com>
2025-01-14 19:33:07 +0100

    new GIN archi

--- src/ggsp/models/encoders.py
+++ src/ggsp/models/encoders.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 from torch_geometric.nn import GINConv, GCNConv, GATConv
 from torch_geometric.nn import global_add_pool
 
+def make_gin_conv(input_dim, out_dim):
+    return GINConv(nn.Sequential(nn.Linear(input_dim, out_dim), nn.ReLU(), nn.Linear(out_dim, out_dim)))
 
 class GIN(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
@@ -52,33 +54,6 @@ class GIN(torch.nn.Module):
         out = self.fc(out)
         return out
 
-class GCN(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
-        super().__init__()
-        self.dropout = dropout
-
-        self.convs = torch.nn.ModuleList()
-        self.convs.append(GCNConv(input_dim, hidden_dim))
-        for layer in range(n_layers - 1):
-            self.convs.append(GCNConv(hidden_dim, hidden_dim))
-
-        self.bn = nn.BatchNorm1d(hidden_dim)
-        self.fc = nn.Linear(hidden_dim, latent_dim)
-
-    def forward(self, data):
-        edge_index = data.edge_index
-        x = data.x
-
-        for conv in self.convs:
-            x = conv(x, edge_index)
-            x = F.elu(x)
-            x = F.dropout(x, self.dropout, training=self.training)
-
-        out = global_add_pool(x, data.batch)
-        out = self.bn(out)
-        out = self.fc(out)
-        return out
-
 class GAT(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
         super().__init__()
@@ -105,3 +80,36 @@ class GAT(nn.Module):
         out = self.bn(out)
         out = self.fc(out)
         return out
+
+class GCN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
+        super(GCN, self).__init__()
+        self.layers = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+
+        for i in range(n_layers):
+            if i == 0:
+                self.layers.append(make_gin_conv(input_dim, hidden_dim))
+            else:
+                self.layers.append(make_gin_conv(hidden_dim, hidden_dim))
+            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
+
+        project_dim = hidden_dim * n_layers
+        self.project = torch.nn.Sequential(
+            nn.Linear(project_dim, project_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(project_dim, project_dim))
+        
+        self.fc = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x, edge_index, batch):
+        z = x
+        zs = []
+        for conv, bn in zip(self.layers, self.batch_norms):
+            z = conv(z, edge_index)
+            z = F.relu(z)
+            z = bn(z)
+            zs.append(z)
+        gs = [global_add_pool(z, batch) for z in zs]
+        z, g = [torch.cat(x, dim=1) for x in [zs, gs]]
+        return z, g
